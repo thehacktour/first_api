c500ffcd4bfdb22dce18439b7cc386f2bf7ee87e
Luara Lima <luaralima205@gmail.com>
2023-08-07 23:28:19 -0300

    atualizações

--- firstapi/settings.py
+++ firstapi/settings.py
@@ -20,9 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
+import os
 
-SECRET_KEY = config('API_TOKEN')
+SECRET_KEY = os.environ.get('API_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
