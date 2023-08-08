8924a4c26d0c6de38b1b192c68982bb6f14e2b75
Luara Lima <luaralima205@gmail.com>
2023-08-08 16:20:21 -0300

    atualizações

--- firstapi/settings.py
+++ firstapi/settings.py
@@ -24,11 +24,12 @@ import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
+PORT = int(os.environ.get("PORT", 8000))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://listbooks-api.onrender.com/']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
--- firstapi/urls.py
+++ firstapi/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls'), name='Home'),
+    path('', include('api.urls')),
 ]
